80d24fe35f3f19f111ab957add44ce53c4495033
arseniisk <arseniy2011@gmail.com>
2021-05-15 17:07:34 -0400

    Set up buefy and font awesome libraries Add configuration files: editorconfig and prettier Add development data set

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Buefy from "buefy";
+import "@/scss/main.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(Buefy, {
+  defaultIconPack: "fas",
+});
 
 new Vue({
   router,
